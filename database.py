ddc04cc69e2cab035d795c59476fcb4f7969232d
Verderen <wwipolii@yandex.ru>
2025-08-12 12:16:53 +0300

    Update database.py

--- database.py
+++ database.py
@@ -8,12 +8,15 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 
 DB_CONFIG = {
     'dbname': DB_NAME,
     'user': DB_USER,
     'password': DB_PASS,
-    'host': DB_HOST
+    'host': DB_HOST,
+    'port': DB_PORT,
+    'ssl': "require"
 }
 
 def db_conn():
